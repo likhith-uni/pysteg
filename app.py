e712a6f83eb4a8d89a1fae5b95b6c14c640d49c6
Srilikhith Sajja <srilikhithsajja@Srilikhiths-MacBook-Pro.local>
2022-12-03 10:57:33 +0530

    n

--- app.py
+++ app.py
@@ -3,15 +3,19 @@ import os
 import uuid
 from werkzeug.utils import secure_filename
 import subprocess
+import glob
 
 UPLOAD_FOLDER = os.path.join(os.getcwd(), "uploads")
 OUTPUT = os.path.join(os.getcwd(), "output")
 IMAGES = os.path.join(UPLOAD_FOLDER,'img')
+EXTRACT = os.path.join(UPLOAD_FOLDER,'temp')
+
 TEXTS = os.path.join(UPLOAD_FOLDER,'txt')
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.secret_key = "asdasdiu3020"
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -24,15 +28,13 @@ def allowed_file(filename):
 def index():
     if request.method == 'POST':
         if 'file' not in request.files:
-            flash("No cover file submitted")
-            return redirect(url_for('index'))
+            return "No cover file"
         if 'to_hide' not in request.files:
-            flash("No embed file submitted")
-            return redirect(url_for('index'))
+            return "No embed file submitted"
         file = request.files['file']
         to_hide = request.files['to_hide']
+        password = request.form['password']
         if file.filename == "" or to_hide.filename == "":
-            flash("No selected file")
             return redirect(url_for('index'))
         if file and allowed_file(file.filename) and to_hide:
             ext = file.filename.split('.')[-1]
@@ -45,13 +47,32 @@ def index():
             ############################
             #Hide file
             os.chdir(os.path.join(os.getcwd(), "output"))
-            subprocess.run(['steghide','embed','-ef',"../uploads/txt/"+id+".txt","-cf","../uploads/img/"+filename,"-sf",filename,"-p","1234"])
-            return redirect(url_for('download_file', name=filename))
+            subprocess.run(['steghide','embed','-ef',"../uploads/txt/"+id+".txt","-cf","../uploads/img/"+filename,"-sf",filename,"-p",password])
+            return send_from_directory(OUTPUT,filename,as_attachment=True)
     return render_template('index.html')
 
-@app.route('/uploads/<name>')
-def download_file(name):
-    return send_from_directory(OUTPUT, name)
+
+@app.route('/decode',methods=["GET","POST"])
+def decode():
+    if request.method == "POST":
+        if 'file' not in request.files:
+            return "No cover file"
+        file = request.files['file']
+        filename = file.filename
+        password = request.form['password']
+        id = str(uuid.uuid4())
+        os.chdir(EXTRACT)
+        os.mkdir(id)
+        temp_path = os.path.join(EXTRACT,id)
+        os.chdir(temp_path)
+        file.save(os.path.join(temp_path,filename))
+        subprocess.run(['steghide','extract','-sf',filename,"-p",password])
+        if glob.glob("*.txt") == []:
+            flash('Invalid Password')
+            return redirect(url_for('decode'))
+        return send_from_directory(temp_path,glob.glob("*.txt")[0],as_attachment=True)
+    return render_template('decode.html')
+        
 
 if __name__ == "__main__":
     app.run(
